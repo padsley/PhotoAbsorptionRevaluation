8d434a90e6d4427cc0e4c8a352653fcf5494af58
padsley <padsley@gmail.com>
2025-08-06 15:06:45 -0500

    adding some files which hadn't been included for some reason

---
+++ UseCombinedTa181.sh
@@ -0,0 +1,4 @@
+rm Ta181.csv
+ln -s Ta181_combined.csv Ta181.csv
+#make a soft link to use the Varlamov 181Ta data automatically :)
+#remember to remove the original soft linked file, possums
---
+++ UseKawanoTa181.sh
@@ -0,0 +1,3 @@
+ln -s Ta181_Kawano.csv Ta181.csv
+#make a soft link to use the Varlamov 181Ta data automatically :)
+#remember to remove the original soft linked file, possums
