11b1f7d3c52ed4b7fcb424b46a207e2a7be51d6a
padsley <padsley@gmail.com>
2025-08-06 15:04:38 -0500

    adding the python script for making the comparison of different 92Molly data

---
+++ MoComparisonPlotMaker.py
@@ -0,0 +1,48 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+import pandas as pd
+#pip install corner
+import corner
+from scipy.optimize import curve_fit
+from scipy import optimize
+from scipy import integrate
+#pip install emcee #PA added this
+import emcee
+from IPython.display import display, Math
+
+nucleiData = np.loadtxt("Mo96.csv",delimiter=',')
+
+mask = np.abs(nucleiData[:,0]<24)#truncate to <24 MeV for the gamma-ray energy
+nucleiData = nucleiData[mask]
+
+xdata = nucleiData[1:,0]
+ydata = nucleiData[1:,1]
+dydata = nucleiData[1:,2]
+
+nucleiData2 = np.loadtxt("Mo96_IAEA_database.csv",delimiter=',')
+
+
+
+mask = np.abs(nucleiData2[:,0]<24e6)#truncate to <24 MeV for the gamma-ray energy
+nucleiData2 = nucleiData2[mask]
+
+xdata2 = nucleiData2[1:,0]
+ydata2 = nucleiData2[1:,1]
+#dydata2 = nucleiData2[1:,2]
+for i in range(len(xdata2)):
+    xdata2[i] /= 1e6#eV->MeV
+    ydata2[i] *= 1e3#b->mb
+
+print(nucleiData2)
+
+plt.errorbar(xdata, ydata, yerr=dydata, fmt=".k", capsize=0)
+plt.plot(xdata2,ydata2, color="orange")
+plt.legend(fontsize=14)
+plt.xlim(10, 25)
+plt.xlabel(r'$\gamma$-ray Energy [MeV]')
+plt.ylabel("Cross Section [mb]")
+
+outputFigName = 'figures/MoComparisonPlot.png'
+plt.savefig(outputFigName)
